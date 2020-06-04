5d0f7b6313fbc9f879af1fde9f870b3e281ec6d9
D3sktool <55096242+D3sktool@users.noreply.github.com>
2020-06-04 12:59:04 -0400

    Use the channel_id returned from the search to allow deleting from a entire guild

--- deleteDiscordMessages.js
+++ deleteDiscordMessages.js
@@ -42,7 +42,7 @@
         <label><input id="autoScroll" type="checkbox" checked><small>Auto scroll</small></label> <span></span>
     </div>
     <pre style="margin-top:150px;font-size:0.75rem;font-family:Consolas,Liberation Mono,Menlo,Courier,monospace;">
-        <center>Star this project on <a href="https://github.com/victornpb/deleteDiscordMessages" target="_blank">github.com/D3sktool/deleteDiscordMessages</a>!\n\n
+        <center>Star this project on <a href="https://github.com/D3sktool/deleteDiscordMessages" target="_blank">github.com/D3sktool/deleteDiscordMessages</a>!\n\n
             <a href="https://github.com/D3sktool/deleteDiscordMessages/issues" target="_blank">Issues or help</a></center>
         </pre></body></html>`);
 
